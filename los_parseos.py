6d288d8abd65d5c4468cd59c1c8e36fd06c4bec1
EnzoD12 <baezenzo419@gmail.com>
2024-01-19 19:50:55 -0300

    la verdad

--- los_parseos.py
+++ los_parseos.py
@@ -1,12 +1,17 @@
 nombre = "Enzo"
-apellido = "Acosta"
+apellido = "Acosta jr"
 edad = 19 
 peso = 68
 
+# array
+
+print("El nombre es {}, el apellido{}".format(nombre, apellido))
 print(type(nombre))
 print(type(edad))
 print(type(peso))
 
 # intentar el parsing
 edad_en_string = str(edad)
-print(f"La edad parseada de este person  {type(edad_en_string)}")
+print(f"""La edad anterior era {type(edad)},
+      La edad parseada de este person  
+      {type(edad_en_string)}""")
