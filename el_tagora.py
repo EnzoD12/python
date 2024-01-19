f5a0cc0d0e8ddc0ae1e2bfec26e818cc9d098db9
EnzoD12 <baezenzo419@gmail.com>
2024-01-19 19:34:05 -0300

    El parseo

---
+++ el_tagora.py
@@ -0,0 +1,12 @@
+import math
+
+print("hola, hielo, hierro")
+
+# teorema de pitagora
+catetoa = 2
+catetob = 2
+
+hipo = math.sqrt( pow(catetoa, 2) + pow(catetob, 2))
+
+print(f"La hipotenusa es {hipo}")
+
---
+++ los_parseos.py
@@ -0,0 +1,12 @@
+nombre = "Enzo"
+apellido = "Acosta"
+edad = 19 
+peso = 68
+
+print(type(nombre))
+print(type(edad))
+print(type(peso))
+
+# intentar el parsing
+edad_en_string = str(edad)
+print(f"La edad parseada de este person  {type(edad_en_string)}")
